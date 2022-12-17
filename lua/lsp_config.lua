97b66179857b5a29dc551a1079f1633d47b9499a
Dylan DiGeronimo <ddigeronimo@seer.ai>
2022-12-16 20:11:57 -0500

    Comment out highlight groups that lsp-colors already sets

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -56,9 +56,10 @@ local on_attach = function(client, bufnr)
   -- Set autocommands conditional on server_capabilities
   if client.server_capabilities.documentHighlightProvider then
     vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=DarkMagenta guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+      " In the event that lsp-colors stops working, uncomment these lines to manually update the highlight groups
+      " hi LspReferenceRea cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+      " hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+      " hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
